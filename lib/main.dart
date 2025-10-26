f8268a0a958f13a7c76a3237d8257d5a5f4a35c2
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2025-10-26 09:05:53 -0700

    Added the Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Ball(),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Ask Me Anything'),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Image.asset('images/ball1.png'));
+  }
+}
